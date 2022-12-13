6ae8d41ae02d1a117b66d6dbdb388026a14d97cb
roman <roman@mac-pro.local>
2022-12-14 05:05:49 +0600

    basic fastapi

--- scrapify/src/main.py
+++ scrapify/src/main.py
@@ -1,2 +1,14 @@
+import typing as tp
+
+import fastapi as fa
+
+app = fa.FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"app": "scrapify"}
+
+
 if __name__ == "__main__":
-    print("Hello World")
+    print("Scrapify")
